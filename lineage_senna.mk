adb525e0d37a55df6d24a27f3d1dd5da4dc42653
pjgowtham <pjgowtham@gmail.com>
2025-08-17 17:45:12 +0530

    senna: Initial rebrand for realme GT Neo5
    Change-Id: I326aabe3c36b87106e567a028a59f37cf7c54603

--- lineage_senna.mk
+++ lineage_senna.mk
@@ -10,23 +10,23 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from lemonadep device
-$(call inherit-product, device/oneplus/lemonadep/device.mk)
+$(call inherit-product, device/realme/senna/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonadep
-PRODUCT_DEVICE := lemonadep
+PRODUCT_NAME := lineage_senna
+PRODUCT_DEVICE := senna
 PRODUCT_MANUFACTURER := OnePlus
-PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2125
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RE5860
 
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OnePlus9Pro-user 14 UKQ1.230924.001 R.1d5a0be-1-4ffee release-keys" \
-    BuildFingerprint=OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.1d5a0be-1-4ffee:user/release-keys \
-    DeviceName=OnePlus9Pro \
-    DeviceProduct=OnePlus9Pro \
-    SystemDevice=OnePlus9Pro \
-    SystemName=OnePlus9Pro
+    BuildDesc="RMX3706-user 15 AP3A.240617.008 T.1d9a49b_20be2-8d release-keys" \
+    BuildFingerprint=realme/RMX3706/RE5860:15/AP3A.240617.008/T.1d9a49b_20be2-8d:user/release-keys \
+    DeviceName=RE5860 \
+    DeviceProduct=RMX3706 \
+    SystemDevice=RE5860 \
+    SystemName=RMX3706
