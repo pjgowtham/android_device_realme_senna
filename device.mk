352f57edc73e4e877dfb8bdf4026de3414f6bee2
pjgowtham <pjgowtham@gmail.com>
2025-09-17 18:23:53 +0530

    senna: Import prebuilt HWC and iris firmware
    Change-Id: I58835ff9e2f52cb8bb0efd66c4880d6919914a19

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2772
 TARGET_SCREEN_WIDTH := 1240
 
+# Display
+TARGET_USES_PREBUILT_HWC := true
+
 # Lineage Health
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
